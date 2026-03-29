7f871806516bd190ac8ff59dad76e6140884b832
sk408 <75577242+sk408@users.noreply.github.com>
2026-03-29 08:48:04 -0700

    feat: add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/hearing_aid_control/",
   plugins: [react()],
   test: {
     environment: "jsdom",
